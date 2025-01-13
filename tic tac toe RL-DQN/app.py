a94578f92a212260227de7fe92947fdcf7d15132
zeyad.h.abaza <zeyad.h.abaza@gmail.com>
2025-01-13 20:14:47 +0200

    add readme file

--- tic tac toe RL-DQN/app.py
+++ tic tac toe RL-DQN/app.py
@@ -6,7 +6,6 @@ import tensorflow as tf
 app = Flask(__name__)
 CORS(app)
 
-# تحميل النموذج المدرب
 model = tf.keras.models.load_model('tic_tac_toe_dnn_model.h5')
 
 @app.route('/')
@@ -15,21 +14,17 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # الحصول على بيانات اللوحة من الطلب
     data = request.get_json()
-    flat_board = np.array(data['board'], dtype=np.float32)  # تحويل اللوحة إلى مصفوفة numpy
-    board = flat_board.reshape(3, 3)  # تحويل اللوحة إلى مصفوفة 3x3
-    board = np.expand_dims(board, axis=-1)  # إضافة قناة واحدة (تصبح الشكل 3x3x1)
-    board = np.expand_dims(board, axis=0)  # إضافة بُعد الدُفعة (تصبح الشكل 1x3x3x1)
+    flat_board = np.array(data['board'], dtype=np.float32)
+    board = flat_board.reshape(3, 3)
+    board = np.expand_dims(board, axis=-1)
+    board = np.expand_dims(board, axis=0)
 
-    # التنبؤ بالحركة التالية باستخدام النموذج
     q_values = model.predict(board, verbose=0)
-    next_move = np.argmax(q_values)  # اختيار الحركة ذات القيمة الأعلى
+    next_move = np.argmax(q_values)
 
-    # التحقق من أن الحركة صالحة (الخلية فارغة)
     if flat_board[next_move] != 0:
-        # إذا كانت الحركة غير صالحة، نبحث عن حركة صالحة
-        sorted_indices = np.argsort(q_values[0])[::-1]  # ترتيب الحركات من الأفضل إلى الأسوأ
+        sorted_indices = np.argsort(q_values[0])[::-1]
         for move in sorted_indices:
             if flat_board[move] == 0:
                 next_move = move
